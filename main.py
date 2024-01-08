fa0a8fbb92b50b5b3857a7455a74d208ab9d2edd
OlegHelgiScribe <oleg_ag@ukr.net>
2024-01-09 00:15:42 +0200

    1 заняття 08,01,2024

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
 print("Hello first commit")
-print("ist work")
-print("ist work 2")
-print("ist work 3")
+print("08.01.2024")
+
+#a=int(input("Введіть число: "))
+#b=int(input("Введіть ще число: "))
+#print(f"Їх сума: {a+b}")
+#print(f"Їх сума: {a+int(b)}")
+
+
+#if a==2:
+#    print(a)
+#else:
+#    #print(f"not {a}")
+#    print(f"not defined")    
+
+
+length = 2.75
+width = 1.75
+area = length*width
+show = f"With width {width} and length {length} of the room, its area is equal to {area}" 
+print(show)  
+
+my_list = [2024, 3.12]
+some_data = ['Python']
+my_list.extend(some_data)
+my_list.insert(1, 'Python')
+print(my_list)
